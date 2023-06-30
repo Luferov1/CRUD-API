44bea996a13d9d9bb2be2501808f064254feec89
Luferov1 <ldima12022002@gmail.com>
2023-06-30 15:31:26 +0300

    refactor: change some settings, add some basic code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello world');
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+const port = process.env.PORT;
+
+const posts = [
+  {
+    title: 'Lorem ipsum',
+    content: 'Dolor sit amet',
+  },
+];
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+  switch (request.url) {
+    case '/api/users': {
+      if (request.method === 'GET') {
+        response.statusCode = 200;
+        response.end(JSON.stringify(posts));
+      }
+      break;
+    }
+    default: {
+      response.statusCode = 404;
+      response.end();
+    }
+  }
+});
+
+server.listen(port);
+
+console.log(`Server is running on PORT: ${port}`);
---
+++ src/utilies/constants.ts
@@ -0,0 +1,7 @@
+export const ENDPOINTS = {
+  USERS: 'api/users',
+};
+
+export const METHODS = {
+
+};
