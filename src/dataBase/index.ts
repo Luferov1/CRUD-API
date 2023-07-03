99a2ade0a16374f8a131e6d591e23d59cbbef66d
Luferov1 <ldima12022002@gmail.com>
2023-07-03 19:07:10 +0300

    feat: add prod script and clear database

--- src/dataBase/index.ts
+++ src/dataBase/index.ts
@@ -1,9 +1,5 @@
 import { User } from '../utilies/models';
 
-const dataBase: User[] = [
-  {
-    username: 'ivan zolo', age: 55, hobbies: ['football', 'gg'], id: 'eb521011-db96-481f-a58e-b6c00197a1fd',
-  },
-];
+const dataBase: User[] = [];
 
 export default dataBase;
