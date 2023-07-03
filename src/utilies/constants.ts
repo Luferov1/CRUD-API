742dde370175e5aa6c48f3231ff1c5e235513c59
Luferov1 <ldima12022002@gmail.com>
2023-07-03 18:59:26 +0300

    feat: add constants

--- src/utilies/constants.ts
+++ src/utilies/constants.ts
@@ -1,7 +1,13 @@
 export const ENDPOINTS = {
-  USERS: 'api/users',
+  USERS: '/api/users',
+  USER: '/api/users/',
 };
 
 export const METHODS = {
-
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
 };
+
+export const UUID = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i;
